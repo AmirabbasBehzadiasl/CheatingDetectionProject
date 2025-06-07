0743e13dc87097e60cc81a29f9111df8ad0218f8
Amirabbas <Amir.bhz.w1@gmail.com>
2025-06-07 21:22:44 +0330

    init project structure

---
+++ spring/src/main/java/com/project/CheatingDetectionProject/CheatingDetectionProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.CheatingDetectionProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheatingDetectionProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheatingDetectionProjectApplication.class, args);
+	}
+
+}
